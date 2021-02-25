cbf9c9fdf9480c8aa93ee03da46275bd581f3759
Lam Hei Tat <albert.htlam@gmail.com>
2021-02-26 01:23:24 +0800

    Update imperative code

--- imperative/main.cpp
+++ imperative/main.cpp
@@ -5,18 +5,18 @@
 int main() {
     printf("Game start!\n");
 
-    /***************************************************************************
-     * Encapsulation:
-     * - Internal states, logics and implementations are hided from outside.
-     * - Caller can only manipulate the object through public methods.
-     * - This facilitate modular design and maintanence of code.
-     **************************************************************************/
-
     printf("\n>>> Round 1 <<<\n\n");
 
     // Create object
     Pokemon a = Create(POKEMON, "Pokemon A");
     Pokemon b = Create(POKEMON, "Pokemon B");
+
+    // Drawback 1:
+    // Caller can modify the struct directly
+    //
+    // The following code are valid but not appropriate:
+    // a.hp = 100;
+    // b.atk = 10;
 
     // Call public methods
     Attack(&a, &b);
@@ -24,18 +24,9 @@ int main() {
     Attack(&a, &b);
     Rest(&b);
 
-    // ERROR: cannot access private or protected members
-    // a.hp = 100;
-    // b.atk = 10;
-
-    /***************************************************************************
-     * Inheritance:
-     * - A subclass inherites all members from it's superclass.
-     * - This facilitate the reuse of code.
-     * - A subclass can add new members and methods, and override existing
-     *   methods.
-     **************************************************************************/
-
+    // Drawback 2:
+    // New or special features have to be implemented by conditional code
+    // in each related function.
     printf("\n>>> Round 2 <<<\n\n");
 
     // Create object of subclass with differnt atk
@@ -45,17 +36,12 @@ int main() {
     // Create object of subclass with new methods
     Pokemon p = Create(PIKACHU, "Pikachu");
     Attack(&p, &b);
+
     Thunderbolt(&p, &b);
 
     // Create object of subclass which overrides existing methods
     Pokemon c = Create(CHARMANDER, "Charmander");
     Attack(&c, &b);
-
-    /***************************************************************************
-     * Polymorphism:
-     * - The same method behaves differently according to its caller's class.
-     * - So each pokemon attacks in their own way!
-     **************************************************************************/
 
     printf("\n>>> Round 3 <<<\n\n");
 
